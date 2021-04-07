f9111f930e932263407bf8f9b7d4c299ad57018c
ginger-kang <kdhoon07@gmail.com>
2021-04-07 19:45:53 +0900

    Head component from next/head

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,12 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>첫 번째 포스트</title>
+      </Head>
       <h1>첫 포스트!</h1>
       <h2>
         <Link href="/">
